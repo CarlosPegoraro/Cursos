a69134eef7afecd5b4d318d60a0818c79268b5fe
Carlos Eduardo Pegoraro Lopes <carlospegorarolopes@gmail.com>
2022-07-19 22:24:34 -0300

    :tada: C#

---
+++ C#/CursoCSharp/POO.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp
+{
+    public class Carro
+    {
+        protected readonly int VelocidadeMaxima;
+        int VelocidadeAtual;
+
+        public Carro( int velocidadeMaxima )
+        {
+            this.VelocidadeMaxima = velocidadeMaxima;
+        }
+
+        protected int AlterarVelocidade( int delta )
+        {
+            int novaVelocidade = this.VelocidadeAtual + delta;
+
+            if (novaVelocidade < 0)
+            {
+                this.VelocidadeAtual = 0;
+            }
+            else if (novaVelocidade > this.VelocidadeMaxima)
+            {
+                this.VelocidadeAtual = this.VelocidadeMaxima;
+            } else
+            {
+                this.VelocidadeAtual = novaVelocidade;
+            }
+
+            return this.VelocidadeAtual;
+        }
+
+        //o virtual serve para poder sobreescrever um metodo
+        public virtual int Acelerar()
+        {
+            return this.AlterarVelocidade(5);
+        }
+
+        public virtual int Frear()
+        {
+            return this.AlterarVelocidade(-5);
+        }
+    }
+
+    public class Uno : Carro
+    {
+        public Uno() : base(200) 
+        {
+
+        }
+    }
+
+    public class Ferrari : Carro
+    {
+        public Ferrari() : base(360)
+        {
+
+        }
+
+        //Overvide serve para sobreescrever um metodo
+        public override int Acelerar()
+        {
+            return this.AlterarVelocidade(-15);
+        }
+
+        public new int Frear()
+        {
+            return this.AlterarVelocidade(-15);
+        }
+    }
+    internal class POO
+    {
+        public static void Executar()
+        {
+        
+        }
+    }
+}
---
+++ C#/CursoCSharp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CursoCSharp.Fundamentos;
+using CursoCSharp.EstruturaDeControle;
+using CursoCSharp.ClassesEMetodos;
+using CursoCSharp.Colecoes;
+using CursoCSharp.MetodosFuncoes;
+
+namespace CursoCSharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                //Fundamentos
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Comentários - Fundamentos", Comentarios.Executar},
+                {"Variáveis e Constantes - Fundamentos", VariaveisEConstantes.Executar},
+                {"Inferencia - Fundamentos", Inferencia.Executar},
+                {"Interlopação de Strings  - Fundamentos", Interpolacao.Executar},
+                {"Notação Ponto - Fundamentos", NotacaoPonto.Executar},
+                {"Lendo Dados - Fundamentos", LendoDados.Executar},
+                {"Formatando Números - Fundamentos", FormatandoNumero.Executar},
+                {"Conversão - Fundamentos", Conversoes.Executar},
+                {"Operadores Aritimeticos - Fundamentos", OperadoresAritimeticos.Executar},
+                {"Operadores Relacionais - Fundamentos", OperadoreRelacionais.Executar},
+                {"Operadores Lógicos - Fundamentos", OperadoresLogicos.Executar},
+                {"Operadores Unários - Fundamentos", OperadoresUnarios.Executar},
+                {"Operador Ternário - Fundamentos", OperadorTernario.Executar},
+                //Estruturas de Controle
+                {"Estrutura If/Else/Else If - Estruturas de Controle", EstruturaIF.Executar},
+                {"Estrutura Switch - Estruturas de Controle", EstruturaSwitch.Executar},
+                {"Estrutura While - Estruturas de Controle", EstruturaWhile.Executar},
+                {"Estrutura Do While - Estruturas de Controle", DoWhile.Executar},
+                {"Estrutura For - Estruturas de Controle", EstruturaFor.Executar},
+                {"Estrutura Foreach - Estruturas de Controle", EstruturaForeach.Executar},
+                //Classes e Métodos
+                {"Membros - Classes e Métodos", Membros.Executar},
+                {"Construtor - Classes e Métodos", Construtor.Executar},
+                {"Métodos Com Retorno - Classes e Métodos", Retorno.Executar},
+                {"Métodos Estáticos - Classes e Métodos", MetodosEstaticos.Executar},
+                {"Atributos Estáticos - Classes e Métodos", AtributosEstaticos.Executar},
+                {"Desafio - Classes e Métodos", DesafiosAtributos.Executar},
+                {"Parametros - Classes e Métodos", ParametrosVariaveis.Executar},
+                {"Parametros Nomeados - Classes e Métodos", ParametrosNomeados.Executar},
+                {"Propriedades - Classes e Métodos", Propriedades.Executar},
+                {"Readonly - Classes e Métodos", Readonly.Executar},
+                {"Enums - Classes e Métodos", ExemploEnum.Executar},
+                {"Struct - Classes e Métodos", ExemploStruct.Executar},
+                //Coleções
+                {"Array - Coleções", ColecoesArray.Executar},
+                {"List - Coleções", ColecoesList.Executar},
+                {"ArrayList - Coleções", ColecoesArrayList.Executar},
+                {"Set - Coleções", ColecoesSet.Executar},
+                {"Igualdade - Coleções", ColecoesDictionary.Executar},
+                //POO
+                {"Programacao Orientada Objetos (Finalizar) - Coleções", POO.Executar},
+                //Metodos e Funcoes
+                {"Metodo Lambda - Coleções", MetodoLambda.Executar},
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
