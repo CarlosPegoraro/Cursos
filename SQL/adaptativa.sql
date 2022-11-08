f7745c0850e2a79512599aabb39c3134a1c87e24
Carlos Eduardo Pegoraro Lopes <carlospegorarolopes@gmail.com>
2022-11-07 21:59:46 -0300

    :tada: index update

---
+++ SQL/adaptativa.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 08-Nov-2022 às 01:15
+-- Versão do servidor: 10.4.24-MariaDB
+-- versão do PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `adaptativa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `empregados`
+--
+
+CREATE TABLE `empregados` (
+  `codigo` int(11) NOT NULL,
+  `nome` varchar(40) NOT NULL,
+  `setor` char(2) NOT NULL,
+  `cargo` varchar(20) NOT NULL,
+  `salario` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `empregados`
+--
+ALTER TABLE `empregados`
+  ADD PRIMARY KEY (`codigo`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
